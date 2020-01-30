19850e84846ddfdffc4a95b37089e5b3149d79d5
Ethan Roy <ethanroy395@gmail.com>
2020-01-30 14:14:12 -0800

    Import and setup basic split frame

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SplitPane from 'react-split-pane'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitPane split="vertical" minSize={50} defaultSize={100}>
+  <div> hello</div>
+  <div> world</div>
+</SplitPane>
   );
 }
 

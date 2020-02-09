6626d1190ee7487d9b6eb8ec97e719ccbd651e42
Ethan Roy <ethanroy395@gmail.com>
2020-02-09 12:43:18 -0800

    Set up linked text boxes with React

--- src/App.js
+++ src/App.js
@@ -5,11 +5,65 @@ import SplitPane from 'react-split-pane'
 
 function App() {
   return (
-    <SplitPane split="vertical" minSize={50} defaultSize={100}>
-  <div> hello</div>
-  <div> world</div>
-</SplitPane>
+   <SplitText/>
   );
+}
+
+class SplitText extends React.Component{
+  //handles the state for both text boxes
+  constructor(props){
+    super(props);
+    this.handleLeftChange = this.handleLeftChange.bind(this);
+    this.handleRightChange = this.handleRightChange.bind(this);
+    this.state={text: '', side: 'left'}
+  }
+
+  handleLeftChange(text){
+    this.setState({side: 'left', text});
+  }
+
+  handleRightChange(text){
+    this.setState({side: 'right', text});
+  }
+
+  render(){
+
+    const side = this.state.side;
+    const text = this.state.text;
+
+    return (
+      <SplitPane split="vertical" minSize={500} defaultSize={500}>
+        <TextInput
+          side = 'left'
+          text = {text}
+          onTextChange = {this.handleLeftChange} />
+       <TextInput
+          side = 'right'
+          text = {text}
+          onTextChange = {this.handleRightChange} />
+      </SplitPane>
+    )
+  }
+}
+
+class TextInput extends React.Component{
+  //individual text boxes that send state to split view
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    }
+
+  handleChange(e) {
+    this.props.onTextChange(e.target.value);
+  }
+
+  render(){
+    const text = this.props.text;
+    const side = this.props.side;
+    return(
+      <textarea value = {text} onChange = {this.handleChange}/>
+    );
+  }
 }
 
 export default App;

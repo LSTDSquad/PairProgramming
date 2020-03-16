23dc570cc312dbd502d01a6021ae1c65ea42707e
Ethan Roy <ethanroy395@gmail.com>
2020-03-16 07:02:34 -0700

    added some comments and updated components

--- src/Components/SplitText.js
+++ src/Components/SplitText.js
@@ -3,16 +3,18 @@ import SplitPane from 'react-split-pane'
 import TextOutput from './TextOutput'
 import TextInput from './TextInput'
 import RunButton from './RunButton';
+import SaveButton from './SaveButton';
 import LoadButton from './LoadButton';
 
 class SplitText extends React.Component{
   //handles the state for both text boxes
+  //state gets managed here (for now?)
   constructor(props){
     super(props);
     this.handleLeftChange = this.handleLeftChange.bind(this);
     this.handleRightChange = this.handleRightChange.bind(this);
     this.handleSessionIDChange = this.handleSessionIDChange.bind(this);
-    this.state={text: '', side: 'left', sessionID: 'unsaved'}
+    this.state={text: '', side: 'left', sessionID: 'unsaved'} //new session will default to 'unsaved' as the session ID
   }
 
   handleLeftChange(text){
@@ -35,15 +37,19 @@ class SplitText extends React.Component{
 
     return (
       <div>
-        <RunButton 
+        <SaveButton 
+          //component to save session to backend
           text = {text} 
           sessionID = {sessionID}/>
         <LoadButton 
+          //component to reload session from session ID
             text = {text}
             sessionID = {sessionID}
             onTextChange = {this.handleLeftChange}
             onSessionIDChange = {this.handleSessionIDChange} />
-        <SplitPane split="vertical" minSize={500} defaultSize={500}>
+        <SplitPane 
+            //One side input, other side output, once we get app to run code?
+            split="vertical" minSize={500} defaultSize={500}>
           <TextInput
             side = 'left'
             text = {text}

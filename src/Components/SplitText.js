284288ae7478f00fce3e7e54bc52b74fd7116cc4
Ethan Roy <ethanroy395@gmail.com>
2020-03-19 11:19:54 -0700

    intial setup with pubnub. not ready yet

--- src/Components/SplitText.js
+++ src/Components/SplitText.js
@@ -5,19 +5,56 @@ import TextInput from './TextInput'
 import RunButton from './RunButton';
 import SaveButton from './SaveButton';
 import LoadButton from './LoadButton';
+import PubNub from 'pubnub';
+import { PubNubProvider, usePubNub} from 'pubnub-react';
+
 
 class SplitText extends React.Component{
   //handles the state for both text boxes
   //state gets managed here (for now?)
   constructor(props){
     super(props);
+
     this.handleLeftChange = this.handleLeftChange.bind(this);
     this.handleRightChange = this.handleRightChange.bind(this);
     this.handleSessionIDChange = this.handleSessionIDChange.bind(this);
-    this.state={text: '', side: 'left', sessionID: 'unsaved'} //new session will default to 'unsaved' as the session ID
+
+    this.state={
+            text: '', 
+            side: 'left', 
+            sessionID: 'unsaved', //new session will default to 'unsaved' as the session ID
+            userID: Math.round(Math.random() * 1000000).toString(),
+            history: []
+          } 
+
+      this.PubNub = new PubNub({
+      subscribe_key: "sub-c-76b1e8e8-6988-11ea-94ed-e20534093ea4",
+      publish_key: "pub-c-94dff15e-b743-4157-a74e-c7270627723b"})
+
+      PubNub.addListener({
+      message: messageEvent => {
+        setMessages([...messages, messageEvent.message]);
+      }
   }
 
-  handleLeftChange(text){
+  componentDidMount() {
+
+
+      // this.PubNub.subscribe({
+      //     channel: 'channel1',
+      //     // message: (message) => console.log("hi", message)
+      //     // // this.setState({
+      //     // //   history: this.state.history.concat(message)
+      //     // // })
+     // });
+
+    //   this.pubnub.getMessage('channel1', (msg) => {
+    //   console.log(msg);
+    // });
+  }
+
+  handleLeftChange(text,message){
+    //sendMessage
     this.setState({side: 'left', text});
   }
 
@@ -29,12 +66,24 @@ class SplitText extends React.Component{
     this.setState({sessionID: sessionID});
   }
 
+  sendMessage(message){
+        console.log('sendMessage')
+        // this.PubNub.publish({
+        //   channel: 'channel1',
+        //   message: message,
+        // });
+  }
+
   render(){
 
     const side = this.state.side;
     const text = this.state.text;
     const sessionID = this.state.sessionID
-
+    const userID = this.state.userID
+    const history = this.state.history
+    
+    const {sendMessage, receiveMessage} = this;
+    
     return (
       <div>
         <SaveButton 
@@ -53,15 +102,21 @@ class SplitText extends React.Component{
           <TextInput
             side = 'left'
             text = {text}
-            onTextChange = {this.handleLeftChange} />
+            onTextChange = {this.handleLeftChange} 
+            sendMessage = {() => this.sendMessage()}
+            userID = {userID}
+            history = {history}/>
           <TextInput 
             side = 'right'
             text = {text}
-            onTextChange = {this.handleRightChange}/>        
+            onTextChange = {this.handleRightChange}
+            userID = {userID}
+            history = {history}/>        
         </SplitPane>
       </div>
     )
   }
+
 }
 
 export default SplitText
--- src/Components/TextInput.js
+++ src/Components/TextInput.js
@@ -11,18 +11,28 @@ class TextInput extends React.Component{
     this.handleChange = this.handleChange.bind(this);
     }
 
-  handleChange(e) {
+  handleChange(e, event) {
+    //console.log(event.action)
+    const messageObj = {
+      Who: this.props.userID,
+      What: this.props.text,
+      When: new Date().valueOf(),
+    };
+
+    this.props.sendMessage(messageObj);
     this.props.onTextChange(e);
   }
 
   render(){
     const text = this.props.text;
     const side = this.props.side;
+    console.log("Received Message: ", this.props.history)
     return(
       <AceEditor
           mode="python"
           theme="github"
           onChange={this.handleChange}
+
           name="UNIQUE_ID_OF_DIV"
           value = {text}
           editorProps={{ $blockScrolling: true }}

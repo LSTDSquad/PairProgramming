10aeeda96c0eebc8c08c2182e6eaec95a4234790
Ethan Roy <ethanroy395@gmail.com>
2020-03-19 14:27:34 -0700

    setup intitial pubnub messaging

--- src/Components/SplitText.js
+++ src/Components/SplitText.js
@@ -18,42 +18,37 @@ class SplitText extends React.Component{
     this.handleLeftChange = this.handleLeftChange.bind(this);
     this.handleRightChange = this.handleRightChange.bind(this);
     this.handleSessionIDChange = this.handleSessionIDChange.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
 
     this.state={
-            text: '', 
+            text: 'The quick brown fox jumped over the lazy dog', 
             side: 'left', 
             sessionID: 'unsaved', //new session will default to 'unsaved' as the session ID
             userID: Math.round(Math.random() * 1000000).toString(),
-            history: []
+            cursors: {}
           } 
 
       this.PubNub = new PubNub({
       subscribe_key: "sub-c-76b1e8e8-6988-11ea-94ed-e20534093ea4",
       publish_key: "pub-c-94dff15e-b743-4157-a74e-c7270627723b"})
 
-      PubNub.addListener({
-      message: messageEvent => {
-        setMessages([...messages, messageEvent.message]);
-      }
-  }
+      this.PubNub.addListener({
+            status: event => {
+              if (event.category === "PNConnectedCategory") {
+              }
+            },
+            message: ({ channel, message}) => {
+              console.log(`Message received in channel: ${channel}`, message.What);
 
-  componentDidMount() {
+              this.setState(({...this.state.cursors[message.Who]=message.What}));
+              console.log(this.state.cursors)
+            }
+          });
 
+      this.PubNub.subscribe({channels: ["channel1"]});
+    }
 
-      // this.PubNub.subscribe({
-      //     channel: 'channel1',
-      //     // message: (message) => console.log("hi", message)
-      //     // // this.setState({
-      //     // //   history: this.state.history.concat(message)
-      //     // // })
-     // });
-
-    //   this.pubnub.getMessage('channel1', (msg) => {
-    //   console.log(msg);
-    // });
-  }
-
-  handleLeftChange(text,message){
+  handleLeftChange(text){
     //sendMessage
     this.setState({side: 'left', text});
   }
@@ -67,11 +62,15 @@ class SplitText extends React.Component{
   }
 
   sendMessage(message){
-        console.log('sendMessage')
-        // this.PubNub.publish({
-        //   channel: 'channel1',
-        //   message: message,
-        // });
+
+        this.PubNub.publish( {channel:'channel1', 
+                              message: message}, function(status, response) {
+                                  console.log('Publish Result: ', status, message)
+        });
+  }
+
+  componentWillUnmount() {
+    this.PubNub.unsubscribeAll();
   }
 
   render(){
@@ -80,9 +79,7 @@ class SplitText extends React.Component{
     const text = this.state.text;
     const sessionID = this.state.sessionID
     const userID = this.state.userID
-    const history = this.state.history
-    
-    const {sendMessage, receiveMessage} = this;
+    const cursors = this.state.cursors
     
     return (
       <div>
@@ -103,20 +100,20 @@ class SplitText extends React.Component{
             side = 'left'
             text = {text}
             onTextChange = {this.handleLeftChange} 
-            sendMessage = {() => this.sendMessage()}
+            onSendMessage = {this.sendMessage}
             userID = {userID}
             history = {history}/>
-          <TextInput 
+          <TextOutput
             side = 'right'
             text = {text}
             onTextChange = {this.handleRightChange}
+            //onSendMessage = {() => this.sendMessage()}
             userID = {userID}
             history = {history}/>        
         </SplitPane>
       </div>
     )
   }
-
 }
 
 export default SplitText
--- src/Components/TextInput.js
+++ src/Components/TextInput.js
@@ -9,30 +9,36 @@ class TextInput extends React.Component{
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.handleCursorChange = this.handleCursorChange.bind(this);
     }
 
   handleChange(e, event) {
     //console.log(event.action)
-    const messageObj = {
-      Who: this.props.userID,
-      What: this.props.text,
-      When: new Date().valueOf(),
-    };
-
-    this.props.sendMessage(messageObj);
     this.props.onTextChange(e);
+  }
+
+  handleCursorChange(e){
+      const cursorPosition = e.getCursor();
+      const messageObj = {
+          Who: this.props.userID,
+          What: cursorPosition,
+          When: new Date().valueOf()
+        };
+
+    console.log(messageObj)
+
+    this.props.onSendMessage(messageObj);
   }
 
   render(){
     const text = this.props.text;
     const side = this.props.side;
-    console.log("Received Message: ", this.props.history)
     return(
       <AceEditor
           mode="python"
           theme="github"
           onChange={this.handleChange}
-
+          onCursorChange={this.handleCursorChange}
           name="UNIQUE_ID_OF_DIV"
           value = {text}
           editorProps={{ $blockScrolling: true }}

22db7db36a787754e7aa7b9650b0f3338721e109
Ethan Roy <ethanroy395@gmail.com>
2020-03-26 06:36:57 -0700

    Save to DB whenever text is entered

--- src/Components/SaveButton.js
+++ src/Components/SaveButton.js
@@ -14,7 +14,6 @@ class SaveButton extends React.Component{
     	let data = {text: this.props.text};
     	let sessionID = this.props.sessionID;
 
-
     	if(sessionID==='unsaved'){
             //if this is a new session, write new session to dynamoDB
     		const url = 'https://4rvuv13ge5.execute-api.us-west-2.amazonaws.com/dev/setData'
@@ -30,7 +29,7 @@ class SaveButton extends React.Component{
     	else{
             //if this session exists already, update the entry in dynamoDB
     		const url = 'https://4rvuv13ge5.execute-api.us-west-2.amazonaws.com/dev/updateData/'+sessionID
-    		
+    		console.log(url);
     		axios.put(url,data)
     			.then(response => {
     				const message = response.data;
@@ -43,9 +42,8 @@ class SaveButton extends React.Component{
     }
 
     render(){
-    	const text = this.props.text;
     	return(
-      	<button type="button" text = {text} onClick = {this.handleClick}>Save</button>
+      	<button type="button" onClick = {this.handleClick}>Save</button>
     );
   }
 }

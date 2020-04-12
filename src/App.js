dcb1e122718b80205a5ad82114e73278c3cddf66
kailiwang <kailiwang2018@gmail.com>
2020-04-12 14:57:02 -0500

    changed authentication, added hosting

--- src/App.js
+++ src/App.js
@@ -34,5 +34,39 @@ function App() {
   );
 }
 
+const signUpConfig = {
+	header: 'My Customized Sign Up',
+	hideAllDefaults: true,
+	defaultCountryCode: '1',
+	signUpFields: [
+	  {
+		label: 'My name',
+		key: 'name',
+		required: true,
+		displayOrder: 1,
+		type: 'string'
+	  },
+	  {
+		label: 'Email',
+		key: 'email',
+		required: true,
+		displayOrder: 2,
+		type: 'string'
+	  },
+	  {
+		label: 'Password',
+		key: 'password',
+		required: true,
+		displayOrder: 3,
+		type: 'password'
+	  },
+	]
+  };
+  const usernameAttributes = 'Email';
+  
+  export default withAuthenticator(App, {
+	signUpConfig,
+	usernameAttributes
+  });
 
-export default withAuthenticator(App, true);
+// export default withAuthenticator(App, true);

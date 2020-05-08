f3d8efe7c29ef5ed33017546426962fde0b340b5
Ethan Roy <ethanroy395@gmail.com>
2020-05-08 06:59:16 -0700

    record forks in dynamoDB

--- src/Components/CopyButton.js
+++ src/Components/CopyButton.js
@@ -21,6 +21,7 @@ class CopyButton extends React.Component {
       response => {
         let newSession = "/" + response.data.id;
         window.open("/#" + newSession);
+        this.addFork(response.data.id)
         // this.props.onSessionIDChange(response.data.id);
         // this.props.history.push(newSession); //navigate to page referencing copy
       },
@@ -29,6 +30,27 @@ class CopyButton extends React.Component {
       }
     );
   };
+
+  addFork = child => {
+
+    console.log(this.props.sessionID, child);
+
+    const userURL = ENDPOINT + "updateChildren/" + this.props.sessionID;
+
+        let sessionID = this.state.sessionID;
+        let data = { child: child };
+
+        axios.put(userURL, data).then(
+          response => {
+            const message = response.data;
+            console.log(message);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+  }
+
 
   render() {
     return (
